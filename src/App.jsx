b13b953541cd422884f7927abc2b0d412717257b
YashwantNG <gawandeyashwant7@gmail.com>
2023-01-11 22:37:50 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FiChevronRight, FiChevronLeft, FiAlertTriangle } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
 function App() {
-  return <h2>slider project setup</h2>
+  const [people, setPeople] = userState(data)
+  const [index, setIndex] = useState(0)
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person
+
+
+          return (
+            <article key={id}>
+              <img src={name} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          )
+        })}
+        <button className="prew">
+          <FiChevronLeft />
+        </button>
+        <button className="prew">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
 }
 
 export default App
